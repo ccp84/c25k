8c0b88462564c46540ee19e2b8d86b10ef228627
ccp84 <ccp84@icloud.com>
2023-02-28 11:59:56 +0000

    py>add run table to models

--- register/models.py
+++ register/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Run(models.Model):
+    title = models.CharField(max_length=80)
+    leader = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='run_leader')
+    location = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    details = models.TextField(blank=True)
+    runners = models.ManyToManyField(
+        User, related_name='run_signup', blank=True)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.title + self.date
+
+    def num_of_runners(self):
+        return self.runners.count()
