8d0133bbad670d78d28ed05d3b065f0854f56a6e
ccp84 <ccp84@icloud.com>
2023-02-28 16:09:51 +0000

    html>add loop for run list to home page

--- register/views.py
+++ register/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.views.generic import ListView
+from .models import Run
 
 
-def home(request):
-    return render(request, "home.html")
+class RunList(ListView):
+    model = Run
+    queryset = Run.objects.all().order_by('-date')
+    template = 'home.html'
