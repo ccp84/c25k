496586288ea1921ee56c2d7c26bdf2779ba96c30
ccp84 <ccp84@icloud.com>
2023-02-28 10:09:15 +0000

    py>add home view

--- register/views.py
+++ register/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
