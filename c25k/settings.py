e7d7c52df6595f98eb5abd2f1a744827ff963973
ccp84 <ccp84@icloud.com>
2023-03-04 15:27:40 +0000

    py>update standard allauth sign up form

--- c25k/settings.py
+++ c25k/settings.py
@@ -57,6 +57,7 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_SIGNUP_FORM_CLASS = 'register.forms.SignupForm'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
